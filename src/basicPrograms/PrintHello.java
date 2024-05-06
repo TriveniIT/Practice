e68718bb5d63f3c7eff5927fd5e6d05320275cb9
A <arvind.gidde@datatusk.com>
2024-05-06 11:27:37 +0530

    Third Commit

--- src/basicPrograms/PrintHello.java
+++ src/basicPrograms/PrintHello.java
@@ -4,6 +4,6 @@ public class PrintHello {
 
 	public static void main(String[] args) {  // Type main and press Ctrl+space.
 		System.out.println("Hello");  // Tyep sysout and press Ctrl+Space
-		
+		System.out.println("Hello2");
 	}
 }

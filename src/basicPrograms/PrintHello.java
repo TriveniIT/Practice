9e83dbd563f4dc59b1f15f0d46b4105dd4a6f75c
A <arvind.gidde@datatusk.com>
2024-05-06 11:55:00 +0530

    Added static method program

--- src/basicPrograms/PrintHello.java
+++ src/basicPrograms/PrintHello.java
@@ -5,5 +5,6 @@ public class PrintHello {
 	public static void main(String[] args) {  // Type main and press Ctrl+space.
 		System.out.println("Hello");  // Tyep sysout and press Ctrl+Space
 		System.out.println("Hello2");
+		System.out.println("Test"+ "\n" +  "Test1"+ "\n"+ "Test2");  // sysout and Ctrl +Space
 	}
 }
---
+++ src/basicPrograms/StaticMethodCalling.java
@@ -0,0 +1,17 @@
+package basicPrograms;
+
+public class StaticMethodCalling {
+
+	public static void main(String[] args) {
+		
+		test();
+		demo();
+		
+	}
+	public static void test() {
+		System.out.println("Static Method");
+	}
+	public static void demo() {
+		System.out.println("Static Mathod 2");
+	}
+}

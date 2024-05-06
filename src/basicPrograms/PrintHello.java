44c4385a97056d9ad00148601de7ad3bcd47975f
A <arvind.gidde@datatusk.com>
2024-05-06 11:03:46 +0530

    Added Hello Print Program

---
+++ src/basicPrograms/PrintHello.java
@@ -0,0 +1,9 @@
+package basicPrograms;
+
+public class PrintHello {
+
+	public static void main(String[] args) {  // Type main and press Ctrl+space.
+		System.out.println("Hello");  // Tyep sysout and press Ctrl+Space
+		
+	}
+}
